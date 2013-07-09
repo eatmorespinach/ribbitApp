c8a7e46cf07afd8d56157b52349eb2d28c137343
Drew Sing <sing.drew@gmail.com>
2013-07-09 19:33:17 -0400

    1st initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RibbitApp::Application.config.secret_token = '805a997cc1e08ec0816a9417e255ad7b7a36fc3645e90378232a799b1483b596d036c7a155b93b85d646a0f1a95a6f52edded23628565d7ec32d87f9c2a3857b'
